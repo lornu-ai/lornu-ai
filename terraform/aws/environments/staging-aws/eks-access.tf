2896f1bf3a389bec232e55ee43793e46699289a4
Steven Irvin <stevei101@users.noreply.github.com>
2025-12-25 05:25:50 -0600

    fix(yaml): correct syntax in Kustomize workflow

---
+++ terraform/aws/environments/staging-aws/eks-access.tf
@@ -0,0 +1,20 @@
+
+resource "aws_eks_access_entry" "github_actions" {
+  cluster_name  = aws_eks_cluster.main.name
+  principal_arn = aws_iam_role.github_actions.arn
+  type          = "STANDARD"
+
+  tags = {
+    Environment = "staging"
+  }
+}
+
+resource "aws_eks_access_policy_association" "github_actions" {
+  cluster_name  = aws_eks_cluster.main.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = aws_iam_role.github_actions.arn
+
+  access_scope {
+    type = "cluster"
+  }
+}
