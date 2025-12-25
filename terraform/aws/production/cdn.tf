797c4bbf3256027872733c6583dfecb41adbe944
Steven Irvin <stevei101@users.noreply.github.com>
2025-12-24 18:54:40 -0600

    fix(cdn): use static for_each keys with toset to resolve plan-time dependency error

--- terraform/aws/production/cdn.tf
+++ terraform/aws/production/cdn.tf
@@ -74,19 +74,13 @@ resource "aws_acm_certificate" "cloudfront" {
 }
 
 resource "aws_route53_record" "cloudfront_cert_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.cloudfront.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+  for_each = toset([var.api_domain])
 
   zone_id = local.route53_zone_id
-  name    = each.value.name
-  type    = each.value.type
+  name    = aws_acm_certificate.cloudfront.domain_validation_options[0].resource_record_name
+  type    = aws_acm_certificate.cloudfront.domain_validation_options[0].resource_record_type
   ttl     = 60
-  records = [each.value.record]
+  records = [aws_acm_certificate.cloudfront.domain_validation_options[0].resource_record_value]
 
   depends_on = [aws_acm_certificate.cloudfront]
 }
