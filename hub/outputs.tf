932344fceb11a716332e5ed1467e620c7eef95f2
Steven Irvin <stevei101@users.noreply.github.com>
2025-12-27 11:32:01 -0600

    update hub/outputs.tf for provider error 29343

--- hub/outputs.tf
+++ hub/outputs.tf
@@ -5,7 +5,7 @@ output "hub_project_id" {
 
 output "workload_identity_provider" {
   description = "The full identifier of the OIDC provider"
-  value       = google_iam_workload_identity_pool_provider.github_provider.name
+  value       = google_iam_workload_identity_pool_provider.tfc_provider.name
 }
 
 output "terraform_admin_sa_email" {
